148a56127f45bbb849f4389babdf469807cec41c
Youngsup Kim <idebtor@gmail.com>
2019-02-06 21:24:14 +0900

    updated

---
+++ UsingStaticLib.md
@@ -0,0 +1,45 @@
+# Data Structures
+  Lecture Notes by idebtor@gmail.com
+  --------------
+# About Static Library
+
+A library is a collection of pre-compiled object files that can be linked into your programs via the linker. Examples are the system functions such as printf() and sqrt(). There are two types of external libraries: static library and shared library.
+
+  - A static library has file extension of `.a` (archive file) in Unixes or `.lib` (library) in Windows.
+  - A shared library has file extension of `.so` (shared objects) in Unixes or `.dll` (dynamic link library) in Windows. Because of the advantage of dynamic linking, g++, by default, links to the shared library if it is available. You can list the contents of a library via `nm filename`.
+
+## How to build a static library
+Let's suppose that you have a source file (`nowic.cpp`) to turn it into a static library (`libnowic.a`). Assume that you keep `lib/nowic.cpp` and `~/include/nowic.h`.
+
+```
+g++ -c nowic.cpp -o nowic.o -I../include
+ar rcs libnowic.a nowic.o
+ar 				         // list all the options available
+ar t libnowic.a 	 // list ~.o files archived
+ar x libnowic.a 	 // extract ~.o files archived
+nm nowic.o 		     // list the actual function names in .o file
+```
+#### ar flags: 		
+  - -r: Insert the files member... into archive (with replacement).
+  - -c: Indicates you are creating an archive file
+  - -s: Write an object-file index into the archive, change is made to the archive 		
+  - -t: display contents of archive
+			(show the list of .o files, use nm ~.o to see functions in ~.o)
+
+#### NOTE
+It is important that you recognize that the g++ compiler requires that you prefix your static library with the keyword `lib` and suffix `.a`, like `libnowic.a`. The `lib` prefix is required by the linker to find the static library.
+
+## How to reference a static library
+Assuming that you are in `~/src` folder and already have `../lib/libnowic.a`  
+
+```
+g++ selection.cpp sortDriver.c -o sort –I../include –L../lib –lnowic
+```
+
+#### g++ flags: 	
+  - -L: Indicates the location of the library you want to reference
+  - -l: Specifies the specific library you want to attach
+
+----------------------------
+_One thing I know, I was blind but now I see. John 9:25_
+----------------------------
--- lib/nowic.cpp
+++ lib/nowic.cpp
@@ -1,134 +1,99 @@
 /****************************************************************************
- * NowIC Library 1.0 - C++ Library for Data Structures and Algorithms
+ * Nowic Library 1.0 - C++ Library for Data Structures and Algorithms
  *
  * Youngsup Kim <idebtor@gmail.com>
  * Compilation:
  * > g++ -c nowic.cpp -o nowic.o -I../include
  * > ar rcs libnowic.a nowic.o
  *
- * Based on Stanford Univ. - Eric Roberts' genlib.c and simpio.c.
- *          Harvard Univ. - Holloway and Malan's cs50.c
- *
- * 02/10/19 Created
+ * 2019/02/05 Created
  *
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
  ***************************************************************************/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include <string.h>
-#include "nowic.h"
+ #include <iostream>
+ #include <sstream>
+ #include <string>
+ #include <cstring>
 
 /*
- * Reads a line of text from standard input and returns the equivalent
- * char; if text does not represent a char, user is prompted to retry.
- * Leading and trailing whitespace is ignored.  If line can't be read,
- * returns CHAR_MAX.
+ * Reads a line of text from standard input and returns it as an int in
+ * the range of [-2^31 + 1, 2^31 - 2], if possible; if text does not
+ * represent such an int, user is prompted to retry. Leading and trailing
+ * whitespace is ignored.  For simplicity, overflow is not detected.
+ * It provides a default parameter, but it can be overwritten.
  */
-char GetChar() {
-    // try to get a char from user
-    while (true) {
-        // get line of text, returning CHAR_MAX on failure
-        cstring line = GetString();
-        if (line == NULL) return CHAR_MAX;
-
-        // return a char if only a char (possibly with
-        // leading and/or trailing whitespace) was provided
-        char c1, c2;
-        if (sscanf(line, " %c %c", &c1, &c2) == 1) {
-            free(line);
-            return c1;
-        }
-        else {
-            free(line);
-            printf("Retry: ");
-        }
-    }
-}
-
+int GetInt(std::string prompt = "Enter an integer: ") {
+   int keyin;
+   std::string line;
+   std::cout << prompt;                   // display a prompt
+   while (true) {
+     if (std::getline(std::cin, line)) {  // get an input
+       std::stringstream sstr(line);      // make sstream obj
+       if (sstr >> keyin) break;          // scan for int
+     }
+     std::cout << "Retry: ";              // not an int, retry
+   }
+   return keyin;
+ }
 
 /*
- * Reads a line of text from standard input and returns it as an
- * int in the range of [-2^31 + 1, 2^31 - 2], if possible; if text
- * does not represent such an int, user is prompted to retry.  Leading
- * and trailing whitespace is ignored.  For simplicity, overflow is not
- * detected.  If line can't be read, returns INT_MAX.
+ * Reads a line of text from standard input and returns the first char.
+ * If text does not represent a char, user is prompted to retry.
+ * Leading and trailing whitespace is ignored.
+ * It provides a default parameter, but it can be overwritten.
  */
-int GetInt(void) {
-    // try to get an int from user
-    while (true) {
-        // get line of text, returning INT_MAX on failure
-        cstring line = GetString();
-        if (line == NULL)
-            return INT_MAX;
-
-        // return an int if only an int (possibly with
-        // leading and/or trailing whitespace) was provided
-        int n; char c;
-		if (sscanf(line, " %d %c", &n, &c) == 1) {
-            free(line);
-            return n;
-        }
-        else {
-            free(line);
-            printf("Retry: ");
-        }
+char GetChar(std::string prompt = "Enter a character: ") {
+  char keyin;
+  std::string line;
+  std::cout << prompt;                  // diplay prompt
+  while (true) {
+    if (getline(std::cin, line)) {      // get a line
+      std::stringstream sstr(line);     // create a sstream obj
+      if (sstr >> keyin) break;         // get the first char
     }
+    std::cout << "Retry: ";             // not a char, retry
+  }
+  return keyin;
 }
-
-
 
 /*
  * Reads a line of text from standard input and returns it as a
- * cstring (char *), sans trailing newline character.  (Ergo, if
- * user inputs only "\n", returns "" not NULL.)  Returns NULL
- * upon error or no input whatsoever (i.e., just EOF).  Leading
- * and trailing whitespace is not ignored.  Stores cstring on heap
- * (via malloc); memory must be freed by caller to avoid leak.
+ * c string (char *), without trailing newline character.
+ * If user inputs only "\n", he/she will be asked to retry.
+ * Leading and trailing whitespace is not ignored.  Stores c string
+ * on heap via malloc and returns the pointer.  The memory must be
+ * freed by caller to avoid leak. Use free(), or delete [] to free it.
  */
-cstring GetString(void) {
-    cstring buffer = NULL;			// growable buffer for chars
-	unsigned int capacity = 0;		// capacity of buffer
-    unsigned int n = 0;				// number of chars actually in buffer
-	int c;							// character read or EOF
 
-    // iteratively get chars from standard input
-    while ((c = fgetc(stdin)) != '\n' && c != EOF) {
-        // grow buffer if necessary
-        if (n + 1 > capacity) {
-            // determine new capacity: start at 32 then double
-            if (capacity == 0)
-                capacity = 32;
-            else if (capacity <= (UINT_MAX / 2))
-                capacity *= 2;
-            else {
-                free(buffer);
-                return NULL;
-            }
+ char* GetString(std::string prompt = "Enter a string: ") {
+   std::string line, str;
+   std::cout << prompt;                 // display prompt
+   while (true) {
+     if (getline(std::cin, line)) {     // get a line
+       std::stringstream ssobj(line);   // make string stream obj
+       if (ssobj >> str) break;         // scan for a string
+     }
+     std::cout << "Retry: ";            // if not, retry
+   }
+   char *cstr = new char [str.length()+1]; // malloc for c char array
+   strcpy (cstr, str.c_str());          // copy the contents
+   return cstr;
+ }
 
-            // extend buffer's capacity
-            cstring temp = (cstring)realloc(buffer, capacity * sizeof(char));
-            if (temp == NULL) {
-                free(buffer);
-                return NULL;
-            }
-            buffer = temp;
-        }
-		buffer[n++] = c;        // append current character to buffer
-    }
+// For the sake of a simple testing
+#if 0
+int main() {
+  int i = GetInt();
+  std::cout << "i=" << i << "===" << std::endl;
 
-	if (n == 0 && c == EOF)		// return NULL if user provided no input
-        return NULL;
+  char ch = GetChar();
+  std::cout << "ch=" << ch << "===" << std::endl;
 
-    // minimize buffer
-    cstring minimal = (cstring)malloc((n + 1) * sizeof(char));
+  char *str = GetString();
+  std::cout << "keyin=" << str << "===" << std::endl;
+  delete[] str;
 
-    strncpy(minimal, buffer, n);
-    free(buffer);
-
-	minimal[n] = '\0';			// terminate cstring
-    return minimal;				// return cstring
+  std::cout << "Happy Coding~~\n";
 }
-
+#endif
