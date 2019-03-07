e2d855c6d4ade709cfcd7b63d103b9860fbfbca4
Youngsup Kim <idebtor@gmail.com>
2019-03-07 16:28:23 +0900

    Update HelloWho.cpp

--- psets/pset01hello/HelloWho.cpp
+++ psets/pset01hello/HelloWho.cpp
@@ -23,16 +23,16 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // Use setvbuf() to prevent the output from buffered on console.
-    // setvbuf(stdout, NULL, _IONBF, 0);
+  // Use setvbuf() to prevent the output from buffered on console.
+  // setvbuf(stdout, NULL, _IONBF, 0);
 
 	for (int i = 0; i < argc; i++)
 		cout << argv[i] << "!\n";
 
 	cout << "Hello World!\n";
 
-    // Use system("pause") to prevent the terminal from disappearing
-    // as soon as the program terminates such as Visual Studio sometimes.
-    // system("pause");
-    return EXIT_SUCCESS;
+  // Use system("pause") to prevent the terminal from disappearing
+  // as soon as the program terminates such as Visual Studio sometimes.
+  // system("pause");
+  return EXIT_SUCCESS;
 }
