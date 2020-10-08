7f2c5338b73c9fa66bad827376d01e443e3b7ae5
Youngsup Kim <idebtor@gmail.com>
2020-10-08 17:12:50 +0900

    Update infix.cpp

--- psets/pset06/infix.cpp
+++ psets/pset06/infix.cpp
@@ -61,8 +61,6 @@ void printStack(stack<T> orig) {
 
 	cout << endl;
 }
-#endif
-
 
 // performs arithmetic operations.
 double apply_op(double a, double b, char op) {
@@ -77,7 +75,7 @@ double apply_op(double a, double b, char op) {
 }
 
 // there is a bug...
-int compute(stack<int>& va_stack, stack<char>& op_stack) {
+double compute(stack<int>& va_stack, stack<char>& op_stack) {
 	double left  = va_stack.top(); va_stack.pop();     
 	double right = va_stack.top(); va_stack.pop();
 	char op = op_stack.top(); op_stack.pop();
@@ -88,7 +86,7 @@ int compute(stack<int>& va_stack, stack<char>& op_stack) {
 }
 
 // returns value of expression after evaluation.
-int evaluate(string tokens) {
+double evaluate(string tokens) {
 	DPRINT(cout << ">evaluate: tokens=" << tokens << endl;);
 	stack<int>  va_stack;                 // stack to store operands or values
 	stack<char> op_stack;                 // stack to store operators.
@@ -103,8 +101,8 @@ int evaluate(string tokens) {
 		if (isdigit(tokens[i])) {
 			int ivalue = 0;
 			cout << "your code here: to handle multi-digits(operand)\n";
-			va = tokens[i] - '0';
-			va_stack.push(va);
+			ivalue = tokens[i] - '0';
+			va_stack.push(ivalue);
 		} 
 		else if (tokens[i] == ')') { // compute it, push the result to va_stack
 
