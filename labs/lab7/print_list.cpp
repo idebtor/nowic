5abf3f7c5c57703a61a1fe8fbf2578af07082f97
Youngsup Kim <idebtor@gmail.com>
2020-12-17 13:45:48 +0900

    up

--- labs/lab7/print_list.cpp
+++
@@ -1,7 +0,0 @@
-// print_list by idebtor@gmail.com
-#include <iostream>
-void print_list(int *list, int n) {
-	for (int i = 0; i < n; i++)
-		std::cout << list[i] << " ";
-	std::cout << std::endl;
-}
